88e252db789b7cc80fdc63d19a333628cbc99ed9
Craig van Heerden <u15029779@tuks.co.za>
2021-07-24 16:34:36 +0200

    Added eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import './App.css';
+import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Redirect
-} from "react-router-dom";
+    Redirect,
+} from 'react-router-dom';
 
-import ExplorerPage from "./pages/explorer-page";
-import BlockPage from "./pages/block-page";
-import NotFoundPage from "./pages/not-found-page";
+import ExplorerPage from './pages/explorer-page';
+import BlockPage from './pages/block-page';
+import NotFoundPage from './pages/not-found-page';
 
 function App() {
-  return (
-      <Router>
-      <Switch>
-        <Route path="/explorer" exact={true}>
-          <ExplorerPage />
-        </Route>
-        <Route path="/block/:blockHash" exact={true}>
-          <BlockPage />
-        </Route>
-        <Route path="/" exact={true}>
-          <Redirect to="/explorer"/>
-        </Route>
-        <Route path="*">
-          <NotFoundPage />
-        </Route>
-      </Switch>
-      </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/explorer" exact>
+                    <ExplorerPage />
+                </Route>
+                <Route path="/block/:blockHash" exact>
+                    <BlockPage />
+                </Route>
+                <Route path="/" exact>
+                    <Redirect to="/explorer" />
+                </Route>
+                <Route path="*">
+                    <NotFoundPage />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
